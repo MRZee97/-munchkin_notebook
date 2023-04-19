1584bd2ee42b94826d55d67228a593fb1781f145
MRZee97 <mihazinurov@gmail.com>
2023-04-19 22:45:03 +0300

    main page

---
+++ lib/features/create_game/presentation/page/create_game_page.dart
@@ -0,0 +1,55 @@
+import 'package:auto_route/auto_route.dart';
+import 'package:flutter/material.dart';
+import 'package:munchkin/core/ui/constants/app_colors.dart';
+import 'package:munchkin/core/ui/widgets/base_page.dart';
+import 'package:munchkin/core/ui/widgets/image_widget.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:munchkin/core/ui/widgets/primary_button.dart';
+import 'package:munchkin/core/ui/widgets/secondary_button.dart';
+import 'package:munchkin/navigation/router.gr.dart';
+
+@RoutePage()
+class CreateGamePage extends StatelessWidget {
+  const CreateGamePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      child: BasePage(
+          title: AppLocalizations.of(context)!.createGame,
+          body: Column(children: [
+            const SizedBox(height: 20),
+            ImageWidget(
+              image: AppLocalizations.of(context)!.logoImage,
+            ),
+            const SizedBox(height: 20),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10),
+              child: Text(
+                AppLocalizations.of(context)!.textCreateGame,
+                style: const TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.w400,
+                    color: AppColors.titleColor,
+                    fontFamily: "academy"),
+              ),
+            ),
+          ]),
+          actions: Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              PrimaryButton(
+                  text: AppLocalizations.of(context)!.createGame,
+                  onPressed: () {
+                    AutoRouter.of(context).push(const MaxLevelRoute());
+                  }),
+              const SizedBox(height: 20),
+              SecondaryButton(
+                  text: AppLocalizations.of(context)!.gameButtonCreated,
+                  onPressed: () {}),
+              const SizedBox(height: 38),
+            ],
+          )),
+    );
+  }
+}
