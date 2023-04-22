e7dd595ad1d47c3dc0773e906ed28bb04f2535b8
MRZee97 <mihazinurov@gmail.com>
2023-04-22 18:31:45 +0300

    curtain

--- lib/features/base_page/presentation/page/base_page.dart
+++ lib/features/base_page/presentation/page/base_page.dart
@@ -13,43 +13,53 @@ class BasePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Container(
-          color: Colors.black,
-          width: double.infinity,
-          child: Container(
-            margin: EdgeInsets.all(MediaQuery.of(context).size.width * 0.05),
-            decoration: const BoxDecoration(
-              color: AppColors.background,
-              borderRadius: BorderRadius.all(Radius.circular(40)),
-            ),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Column(
-                  children: [
-                    const SizedBox(height: 40),
-                    Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 20),
-                      child: Text(
-                        title,
-                        textAlign: TextAlign.center,
-                        style: const TextStyle(
-                          fontSize: 32,
-                          fontWeight: FontWeight.w700,
-                          color: AppColors.titleColor,
+      body: Column(
+        children: [
+          Container(
+            height: MediaQuery.of(context).viewPadding.top,
+            color: Colors.black,
+          ),
+          Container(
+            color: Colors.black,
+            width: double.infinity,
+            height: MediaQuery.of(context).size.height -
+                MediaQuery.of(context).viewPadding.top,
+            child: Container(
+              margin: EdgeInsets.all(
+                MediaQuery.of(context).size.width * 0.05,
+              ),
+              decoration: const BoxDecoration(
+                color: AppColors.background,
+                borderRadius: BorderRadius.all(Radius.circular(40)),
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Column(
+                    children: [
+                      const SizedBox(height: 40),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 20),
+                        child: Text(
+                          title,
+                          textAlign: TextAlign.center,
+                          style: const TextStyle(
+                            fontSize: 32,
+                            fontWeight: FontWeight.w700,
+                            color: AppColors.titleColor,
+                          ),
                         ),
                       ),
-                    ),
-                    body
-                  ],
-                ),
-                actions,
-              ],
+                      body
+                    ],
+                  ),
+                  actions,
+                ],
+              ),
             ),
           ),
-        ),
+        ],
       ),
     );
   }
